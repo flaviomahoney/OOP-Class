692dd5116e21008af16f7708689bef50fea20bdd
flaviomahoney <63248684+flaviomahoney@users.noreply.github.com>
2021-03-18 14:27:34 -0300

    Add files via upload
    Exercicio1

---
+++ Exercicio1.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("A tabuada de 10 é:");
+    for( int i = 1; i<=9; i++){
+      System.out.println(10*i);
+    }
+  }
+}
